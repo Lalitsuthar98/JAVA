4c94e992eea25ca17cf4e9c38bc5ffe616ccb32c
lalitsuthar98 <ls2817584@gmail.com>
2026-02-18 23:13:50 +0530

    variable_&_datatype

---
+++ Demo.java
@@ -0,0 +1,67 @@
+public class Demo {
+    public static void main(String[] args) { 
+        // integer
+        // byte firstnum = 10; 
+        // short secondnum = 1000;
+        // int thirdnum  = 123456;
+        // long fourth = 45353;  
+        // print method in java  
+        // 1 method  
+        // system.out.println(variable)
+        // System.out.println(firstnum);
+        // System.out.println(secondnum);
+        // System.out.println(thirdnum);
+        // System.out.println(fourth);
+        // 2 method  to print the data  
+        // System.out.print(firstnum);
+        // System.out.print(secondnum);
+        // System.out.print(thirdnum);
+        // System.out.print(fourth); 
+        // 3 Method to print 
+        // System.out.println("the first vaiable is :"+firstnum); 
+
+        // 4 method professional 
+        // System.out.printf("the first variable is %d and second variable is %d",firstnum,secondnum); 
+
+        // float number  
+        // float f = 23.32f;
+        // double d = 12.346;
+        // System.out.println("the float value :" + f +"  double value is : "+d); 
+
+        // charector value 
+        // char c = 'A';
+        // System.err.println("the charector value is :" + c);
+        // boolean variable  
+        // boolean check = true;
+        // System.out.println("the boolean variable is :" +check);  
+
+        // 0b for the binary 
+        // byte b = 0b1010;
+        // System.out.println(b);  
+
+        // 0 for the octal represent 
+        // byte o = 06;
+        // System.out.println(o);
+        
+        // 0x for the hexadecimal 
+        // byte x = 0x10;
+        // System.out.println(x);  
+
+        // declaration   
+        // int x;  
+        // defination 
+        // x = 4;
+        // System.out.println(x);  
+
+        // In java have the 68 keyword  
+        
+
+
+
+
+
+
+
+
+    }
+}
