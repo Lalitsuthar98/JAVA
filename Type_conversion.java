5b3fa642ed5d8adec167e4d0a2f5247af6e70a00
lalitsuthar98 <ls2817584@gmail.com>
2026-02-20 22:47:37 +0530

    type-conversion

---
+++ Type_conversion.java
@@ -0,0 +1,41 @@
+public class Type_conversion {
+    public static void main(String[] args) {
+        // typre conversion is a two type  
+        // 1. Emplicit conversion 
+        // 2. Explicit conversion 
+         
+        // emplicit conversion 
+        // byte i = 23;
+        // int j = i;
+        // System.out.println(j);   // emplicit 
+
+        // Explicit conversion 
+        // int i = 300; 
+        // byte j = (byte)i;
+        // System.out.println(j);
+        // char c = 'A';
+        // int i = c;
+        // System.out.println(i);
+        
+        // truncating conversion 
+        // float ---> int 
+        // double ----> int  
+
+        // float decimal = 121.12f;
+        // int value  = (int)decimal;
+        // System.out.println(value);  
+
+        // double decimal = 12323.343;
+        // int value = (int)decimal;
+        // System.out.println(value);
+        
+        // Automatic type promotion 
+        // byte b = 50;
+        // b =(byte) (b*2);  
+        // System.out.println(b);
+        
+        // Important  order  
+
+        // byte → short → int → long → float → double  
+    }
+}
